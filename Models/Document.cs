311fb4d08bb27ab504fda9f32ad295115680ed2c
st10260039 <ST10260039@VCCONNECT.EDU.ZA>
2025-10-23 16:29:48 +0200

    Document model has been coded up to work hand in hand with upload document feature added

--- Models/Document.cs
+++ Models/Document.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace MonthlyClaimSystem.Models
+{
+
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        public int ClaimId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string FileType { get; set; }
+        public byte[] EncryptedData { get; set; }
+    }
+}
